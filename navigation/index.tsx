a3ec7d3cdd5a57b672403fbfbe7ec953415f04a2
Dian Rahmadani <madiajijah7@gmail.com>
2022-02-18 21:01:26 +0800

    Update index.tsx

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -14,7 +14,7 @@ import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
-import HomeScreen from '../screens/HomeScreen';
+import HomeScreen from '../screens/HomeScreen/Index';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
