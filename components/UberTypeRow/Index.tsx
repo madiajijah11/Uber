01285b37f1f14024af3b1ebf8df953baada7be01
Dian Rahmadani <madiajijah7@gmail.com>
2022-02-19 17:37:48 +0800

    styling UberTypeRow

--- components/UberTypeRow/Index.tsx
+++ components/UberTypeRow/Index.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 
+import { Ionicons } from "@expo/vector-icons";
+
 import styles from "./styles";
 
 const UberTypeRow = () => {
     return (
-        <View>
+        <View style={styles.container}>
             {/* Images */}
             <Image source={require("../../assets/images/UberX.jpeg")} style={styles.image} />
-            <View style={styles.middleContainer}></View>
-            <View style={styles.rightContainer}></View>
+            <View style={styles.middleContainer}>
+                <Text style={styles.type}>
+                    UberX{" "}
+                    <Ionicons name="people" size={16} color="black" />
+                    3
+                </Text>
+                <Text style={styles.time}>
+                    3:00 PM dropoff
+                </Text>
+            </View>
+            <View style={styles.rightContainer}>
+                <Ionicons name="pricetag" size={18} color="green" />
+                <Text style={styles.price}>
+                    $8.00
+                </Text>
+            </View>
         </View>
     );
 }
--- components/UberTypeRow/styles.tsx
+++ components/UberTypeRow/styles.tsx
@@ -1,9 +1,38 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    image: {},
-    middleContainer: {},
-    rightContainer: {}
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 20,
+    },
+    image: {
+        width: 50,
+        height: 50,
+        resizeMode: "contain",
+    },
+    middleContainer: {
+        flex: 1,
+        marginHorizontal: 10,
+    },
+    rightContainer: {
+        width: 100,
+        justifyContent: "flex-end",
+        flexDirection: "row",
+    },
+    type: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 3,
+    },
+    time: {
+        color: "grey",
+    },
+    price: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginLeft: 5,
+    }
 });
 
 export default styles;
