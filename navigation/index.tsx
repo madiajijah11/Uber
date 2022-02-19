41a67ffa3df24ecf5bdd8d73ec6dd4b5118fa53e
Dian Rahmadani <madiajijah7@gmail.com>
2022-02-19 14:33:33 +0800

    add route
    destination page

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -15,7 +15,7 @@ import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import HomeScreen from '../screens/HomeScreen/Index';
-import TabTwoScreen from '../screens/TabTwoScreen';
+import DestinationSearch from '../screens/DestinationSearch/Index';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -85,10 +85,10 @@ function BottomTabNavigator() {
         })}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
+        name="Destination"
+        component={DestinationSearch}
         options={{
-          title: 'Tab Two',
+          title: 'Destination Page',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
