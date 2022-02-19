a0e7eaf592cafac2b36a4f2fda0a2b347265e749
Dian Rahmadani <madiajijah7@gmail.com>
2022-02-19 17:12:45 +0800

    add new file images

--- components/UberTypeRow/Index.tsx
+++ components/UberTypeRow/Index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import styles from "./styles";
 
 const UberTypeRow = () => {
     return (
         <View>
-            <Text>Hello</Text>
+            {/* Images */}
+            <Image source={require("../../assets/images/UberX.jpeg")} style={styles.image} />
+            <View style={styles.middleContainer}></View>
+            <View style={styles.rightContainer}></View>
         </View>
     );
 }
