9004ab2e75ea3699072b7576c1a19366e91e450e
SK <sarah@nnenu.com>
2022-02-20 14:19:33 +0800

    update map style
    - need to make a height for the <View> to wrap the <MapView>

--- components/HomeMap/styles.tsx
+++ components/HomeMap/styles.tsx
@@ -2,14 +2,17 @@ import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        // height: 300,
+        height: Dimensions.get('window').height/3,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
     },
     map :{
         width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        height: Dimensions.get('window').height/3,
+        // width: 100,
+        // height: 100,
     }
 });
 
