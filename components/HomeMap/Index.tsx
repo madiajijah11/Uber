b7beb1b8f7d60da9723829b33be6b901347d070e
Dian Rahmadani <madiajijah7@gmail.com>
2022-02-20 18:21:03 +0800

    Update Index.tsx
    add marker

--- components/HomeMap/Index.tsx
+++ components/HomeMap/Index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, Dimensions } from "react-native";
 
-import MapView from "react-native-maps";
+import MapView, {Marker} from "react-native-maps";
 import styles from "./styles";
 
 const HomeMap = () => {
@@ -14,8 +14,10 @@ const HomeMap = () => {
                 longitude: 114.172806,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
-              }}
-            />
+              }}>
+            <Marker coordinate={{ latitude : 22.315713 , longitude : 114.172806 }}
+                image={require("../../assets/images/top-UberX.png")}/>
+            </MapView>
         </View>
     );
 }
